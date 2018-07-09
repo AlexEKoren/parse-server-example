dbeb065fbfa91a86615a2de1d60a6a51d9bee1b3
Alex Koren <alex@actiview.co>
2018-07-09 14:16:19 -0700

    first cloud function

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ var dashboard = new ParseDashboard({
   "allowInsecureHTTP": true,
   "apps": [
     {
-      "serverURL": 'http://ctiplay.herokuapp.com/parse',
+      "serverURL": 'http://actiplay.herokuapp.com/parse',
       "appId": process.env.APP_ID,
       "masterKey": process.env.MASTER_KEY,
       "appName": "Actiplay"
