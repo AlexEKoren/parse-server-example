64bd22739c60eb02199ac867f8b817d17d7629dc
Alex Koren <alex@actiview.co>
2018-07-09 14:37:22 -0700

    first cloud function

--- cloud/main.js
+++ cloud/main.js
@@ -1,10 +1,14 @@
 
 Parse.Cloud.beforeSave("Follow", function(request, response) {
+	if (request.user.id != request.object.get('follower')) {
+		return response.error('You can\'t create a follow for another user!');
+	}
 	var Follow = Parse.Object.extend("Follow");
 	var query = new Parse.Query(Follow);
 	query.equalTo('following', request.object.get('following'));
 	query.equalTo('follower', request.object.get('follower'));
 	query.count({
+		useMasterKey:true,
 		success: function(count) {
 			if (count > 0) {
 				response.error('This follow already exists!');
