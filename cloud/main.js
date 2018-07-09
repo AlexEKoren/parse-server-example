a228e336d4d961f612efebd9a4b6299c8e177e8d
Alex Koren <alex@actiview.co>
2018-07-09 14:42:00 -0700

    first cloud function

--- cloud/main.js
+++ cloud/main.js
@@ -1,8 +1,10 @@
 
 Parse.Cloud.beforeSave("Follow", function(request, response) {
-	if (request.user && request.user.id != request.object.get('follower')) {
+	if (!request.master && request.user && request.user.id != request.object.get('follower').id) {
 		return response.error('You can\'t create a follow for another user!');
 	}
+	if (!request.user)
+		return response.error('You must be logged in to create follows');
 	var Follow = Parse.Object.extend("Follow");
 	var query = new Parse.Query(Follow);
 	query.equalTo('following', request.object.get('following'));
