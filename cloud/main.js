868064f9e5cad797e7a5b0ee0b7844089981db88
Alex Koren <alex@actiview.co>
2018-07-09 14:02:03 -0700

    first cloud function

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,19 @@
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+Parse.Cloud.beforeSave("Follow", function(request, response) {
+	var Follow = Parse.Object.extend("Follow");
+	var query = new Parse.Query(Follow);
+	query.equalTo('following', request.object.get('following'));
+	query.equalTo('follower', request.object.get('follower'));
+	query.count({
+		success: function(count) {
+			if (count > 0) {
+				response.error('This follow already exists!');
+			} else {
+				response.success();
+			}
+		},
+		error: function(error) {
+			response.error('Failed to run the query');
+		}
+	})
 });
