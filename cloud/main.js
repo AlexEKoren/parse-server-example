7388781ac88a9da34ab358cefe4399b7a0872113
Alex Koren <alex@actiview.co>
2018-07-10 09:56:18 -0700

    unfollow cc

--- cloud/main.js
+++ cloud/main.js
@@ -28,9 +28,10 @@ Parse.Cloud.beforeSave("Follow", function(request, response) {
 });
 
 Parse.Cloud.define("unfollow", function(request, response) {
+	console.log('UNFOLLOW: ' + request.params.follower.id);
 	if (!request.user)
 		return response.error('You must be logged in to unfollow');
-	if (request.user && request.user.id != request.object.get('follower').id) {
+	if (request.user && request.user.id != request.params.follower.id) {
 		return response.error('You can\'t unfollow for another user!');
 	}
 	var Follow = Parse.Object.extend("Follow");
