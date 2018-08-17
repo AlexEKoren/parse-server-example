58534ba44813299d89dbd616e981177884b7b85f
Alex Koren <alex@actiview.co>
2018-08-17 11:36:11 -0700

    post badges?

--- cloud/main.js
+++ cloud/main.js
@@ -143,6 +143,7 @@ Parse.Cloud.define('post_event', function(request, response) {
 	}).then(function(badge) {
 		response.success({"event":event, "badge":badge});
 	}, function(error) {
+		console.error(error);
 		response.success({"event":event});
 	});
 });
